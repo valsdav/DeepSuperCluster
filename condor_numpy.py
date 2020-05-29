fc5aa4eefb5250f6427056657382181867541636
Davide Valsecchi <davide.valsecchi@cern.ch>
2020-05-30 00:23:20 +0200

    Removed geomustache requirement, new score

--- condor_numpy.py
+++ condor_numpy.py
@@ -34,7 +34,7 @@ condor = '''executable              = run_numpy_script.sh
 output                  = output/strips.$(ClusterId).$(ProcId).out
 error                   = error/strips.$(ClusterId).$(ProcId).err
 log                     = log/strips.$(ClusterId).log
-transfer_input_files    = cluster_tonumpy_dynamic.py, windows_creator_dynamic.py, calo_association.py
+transfer_input_files    = cluster_tonumpy_dynamic.py, windows_creator_dynamic.py, calo_association.py, simScore_Minima_withHitFraction.root, Mustache.C
 
 +JobFlavour             = "{queue}"
 queue arguments from arguments.txt
