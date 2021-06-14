f597a849ac16aae5990a50d8f5c1f3c30c97806a
Davide Valsecchi <davide.valsecchi@cern.ch>
2021-06-14 10:11:48 +0200

    Saved updates

--- Training/global_model/model.py
+++ Training/global_model/model.py
@@ -778,7 +778,7 @@ def clusters_classification_loss(y_true, y_pred):
     y_clclass, y_windclass, cl_X, wind_X, y_metadata, cl_labels = y_true
         
     class_loss = tf.keras.losses.binary_crossentropy(y_clclass, dense_clclass, from_logits=True) * mask_cls
-    reduced_loss = tf.reduce_mean(tf.reduce_mean(class_loss, axis=-1))
+    reduced_loss = tf.reduce_mean(tf.reduce_sum(class_loss, axis=-1))
     return reduced_loss 
 
 
@@ -786,13 +786,13 @@ def energy_weighted_classification_loss(y_true, y_pred):
     (dense_clclass, dense_windclass, en_regr_factor), mask_cls, _  = y_pred
     y_clclass, y_windclass, cl_X, wind_X, y_metadata, cl_labels = y_true
     cl_ets = cl_X[:,:,1]
-    matched_window = tf.cast(y_metadata[:,-1]!=0, tf.float32)
+    # matched_window = tf.cast(y_metadata[:,-1]!=0, tf.float32)
     # compute the weighting mean of the loss based on the energy of each seed in the window
     cl_ets_weights = cl_ets / tf.reduce_sum(cl_ets, axis=-1)[:,tf.newaxis]
     class_loss = tf.keras.losses.binary_crossentropy(y_clclass, dense_clclass, from_logits=True) * mask_cls
     weighted_loss = class_loss * cl_ets_weights
     # mean over the batch
-    reduced_loss = tf.reduce_sum(tf.reduce_sum(weighted_loss*matched_window, axis=-1)) / tf.reduce_sum(matched_window)
+    reduced_loss = tf.reduce_mean(tf.reduce_sum(weighted_loss, axis=-1))
     return reduced_loss
 
 
@@ -810,23 +810,23 @@ def energy_loss(y_true, y_pred):
     (dense_clclass, dense_windclass, en_regr_factor), mask_cls, _  = y_pred
     y_clclass, y_windclass, cl_X, wind_X, y_metadata, cl_labels = y_true
     y_target = tf.cast(y_clclass, tf.float32) 
-    matched_window = tf.cast(y_metadata[:,-1]!=0, tf.float32)
+    # matched_window = tf.cast(y_metadata[:,-1]!=0, tf.float32)
 
     pred_prob = tf.nn.sigmoid(dense_clclass)
     diff = tf.math.abs(y_target - pred_prob)
-    Et = cl_X[:,:,1:2]
-    missing_en = Et * diff * y_target
-    spurious_en =  Et * diff * (1 - y_target)
     En_true_sim = y_metadata[:,0]
-    reduced_loss_missing = tf.reduce_sum(tf.squeeze(tf.reduce_sum(missing_en, axis=1)) * matched_window / En_true_sim ) /  tf.reduce_sum(matched_window)
-    reduced_loss_spurious =  tf.reduce_sum(tf.squeeze(tf.reduce_sum(spurious_en, axis=1)) * matched_window / En_true_sim) / tf.reduce_sum(matched_window)
+    Et = cl_X[:,:,1:2]
+    missing_en = Et * diff * y_target / En_true_sim
+    spurious_en =  Et * diff * (1 - y_target)  / En_true_sim
+    reduced_loss_missing = tf.reduce_mean(tf.reduce_sum(missing_en, axis=1))
+    reduced_loss_spurious =  tf.reduce_mean(tf.reduce_sum(spurious_en, axis=1))
     return reduced_loss_missing,reduced_loss_spurious
 
 def soft_f1_score(y_true, y_pred):
     (dense_clclass, dense_windclass, en_regr_factor), mask_cls, _  = y_pred
     y_clclass, y_windclass, cl_X, wind_X, y_metadata, cl_labels = y_true
     y_target = tf.cast(y_clclass, tf.float32) 
-    matched_window = tf.cast(y_metadata[:,-1]!=0, tf.float32)
+    # matched_window = tf.cast(y_metadata[:,-1]!=0, tf.float32)
 
     pred_prob = tf.nn.sigmoid(dense_clclass)
     tp = tf.reduce_sum(pred_prob * y_target, axis=1)
@@ -834,7 +834,7 @@ def soft_f1_score(y_true, y_pred):
     fp = tf.reduce_sum(pred_prob * (1 - y_target), axis=1)
 
     soft_f1_loss = 1 - (2 * tp)/ (2*tp + fn + fp + 1e-16) 
-    reduced_f1 = tf.reduce_sum(tf.squeeze(soft_f1_loss) * matched_window) /  tf.reduce_sum(matched_window)
+    reduced_f1 = tf.reduce_mean(soft_f1_loss)
     return reduced_f1
 
 
@@ -842,11 +842,11 @@ def energy_regression_loss(y_true, y_pred):
     (dense_clclass, dense_windclass, en_regr_factor), mask_cls, _  = y_pred
     y_clclass, y_windclass, cl_X, wind_X, y_metadata, cl_labels = y_true
     cl_ens = cl_X[:,:,0]
-    matched_window = tf.cast(y_metadata[:,-1]!=0, tf.float32)
+    # matched_window = tf.cast(y_metadata[:,-1]!=0, tf.float32)
     
     pred_en =  tf.reduce_sum(cl_ens * tf.squeeze(tf.cast(tf.nn.sigmoid(dense_clclass) > 0.5 , tf.float32)), axis=-1)
     corrected_en =  pred_en * tf.squeeze(en_regr_factor*3)
     true_en_gen = y_metadata[:,2]  # en_true_gen
-    MSE = tf.reduce_sum(tf.square(corrected_en - true_en_gen) *matched_window ) / tf.reduce_sum(matched_window)
+    MSE = tf.reduce_mean(tf.square(corrected_en - true_en_gen) )
     return MSE
 
