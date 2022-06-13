2781064f4c65b8d0ac602b3ce597b0788514336d
Davide Valsecchi <davide.valsecchi@cern.ch>
2022-06-13 16:17:53 +0200

    Updating the notebooks with resolutions studies

--- Evaluation/GraphSC/reco_comparison/run_condor_dumper/join_all.sh
+++ Evaluation/GraphSC/reco_comparison/run_condor_dumper/join_all.sh
@@ -2,13 +2,11 @@
 
 ver=$1
 
-python join_datasets.py -i /eos/user/r/rdfexp/ecal/cluster/output_deepcluster_dumper/reco_comparison/supercluster_regression/electrons/ele_UL18_123X_DeepSC_AlgoA_${ver} -o /eos/user/r/rdfexp/ecal/cluster/output_deepcluster_dumper/reco_comparison/supercluster_regression/electrons/ele_UL18_123X_DeepSC_AlgoA_${ver}_{type}.h5py
+python join_datasets.py -i /eos/user/r/rdfexp/ecal/cluster/output_deepcluster_dumper/reco_comparison/supercluster_regression/electrons/ele_UL18_123X_Mustache_v${ver} -o /eos/user/r/rdfexp/ecal/cluster/output_deepcluster_dumper/reco_comparison/supercluster_regression/electrons/ele_UL18_123X_Mustache_v${ver}_{type}.h5py
+
+python join_datasets.py -i /eos/user/r/rdfexp/ecal/cluster/output_deepcluster_dumper/reco_comparison/supercluster_regression/electrons/ele_UL18_123X_DeepSC_AlgoA_v${ver} -o /eos/user/r/rdfexp/ecal/cluster/output_deepcluster_dumper/reco_comparison/supercluster_regression/electrons/ele_UL18_123X_DeepSC_AlgoA_v${ver}_{type}.h5py
 
 
-python join_datasets.py -i /eos/user/r/rdfexp/ecal/cluster/output_deepcluster_dumper/reco_comparison/supercluster_regression/electrons/ele_UL18_123X_Mustache_${ver} -o /eos/user/r/rdfexp/ecal/cluster/output_deepcluster_dumper/reco_comparison/supercluster_regression/electrons/ele_UL18_123X_Mustache_${ver}_{type}.h5py
+python join_datasets.py -i /eos/user/r/rdfexp/ecal/cluster/output_deepcluster_dumper/reco_comparison/supercluster_regression/gammas/gamma_UL18_123X_DeepSC_AlgoA_v${ver} -o /eos/user/r/rdfexp/ecal/cluster/output_deepcluster_dumper/reco_comparison/supercluster_regression/gammas/gamma_UL18_123X_DeepSC_AlgoA_v${ver}_{type}.h5py
 
-
-
-python join_datasets.py -i /eos/user/r/rdfexp/ecal/cluster/output_deepcluster_dumper/reco_comparison/supercluster_regression/gammas/gamma_UL18_123X_DeepSC_AlgoA_${ver} -o /eos/user/r/rdfexp/ecal/cluster/output_deepcluster_dumper/reco_comparison/supercluster_regression/gammas/gamma_UL18_123X_DeepSC_AlgoA_${ver}_{type}.h5py
-
-python join_datasets.py -i /eos/user/r/rdfexp/ecal/cluster/output_deepcluster_dumper/reco_comparison/supercluster_regression/gammas/gamma_UL18_123X_Mustache_${ver} -o /eos/user/r/rdfexp/ecal/cluster/output_deepcluster_dumper/reco_comparison/supercluster_regression/gammas/gamma_UL18_123X_Mustache_${ver}_{type}.h5py
+python join_datasets.py -i /eos/user/r/rdfexp/ecal/cluster/output_deepcluster_dumper/reco_comparison/supercluster_regression/gammas/gamma_UL18_123X_Mustache_v${ver} -o /eos/user/r/rdfexp/ecal/cluster/output_deepcluster_dumper/reco_comparison/supercluster_regression/gammas/gamma_UL18_123X_Mustache_v0${ver}_{type}.h5py
--- Evaluation/GraphSC/reco_comparison/run_condor_dumper/run_all.sh
+++ Evaluation/GraphSC/reco_comparison/run_condor_dumper/run_all.sh
@@ -2,10 +2,15 @@
 ver=$1
 options=" --loop-on-calo"
 
+
+basedir="/eos/cms/store/group/dpg_ecal/alca_ecalcalib/bmarzocc/Clustering/"
+outdir="/eos/user/r/rdfexp/ecal/cluster/output_deepcluster_dumper/reco_comparison/supercluster_regression/"
+
+
 mkdir ele_must
 cd ele_must
 mkdir error log output
-python ../condor_run_dumper.py -i /eos/cms/store/group/dpg_ecal/alca_ecalcalib/bmarzocc/Clustering/FourElectronsGunPt1-100_pythia8_StdMixing_Flat55To75_14TeV_123X_mcRun3_2021_realistic_v11_UL18_pfRechitThres_SCRegression_Mustache_Dumper  -o /eos/user/r/rdfexp/ecal/cluster/output_deepcluster_dumper/reco_comparison/supercluster_regression/electrons/ele_UL18_123X_Mustache_v${ver}/ -a sim_fraction --wp-file simScore_Minima_ElectronsOnly_updated.root -nfg 30 -q espresso --compress ${options}
+python ../condor_run_dumper.py -i ${basedir}/FourElectronsGunPt1-100_pythia8_StdMixing_Flat55To75_14TeV_123X_mcRun3_2021_realistic_v11_UL18_pfRechitThres_Dumper_SCRegression_Mustache_125X_bugFix  -o ${outdir}electrons/ele_UL18_123X_Mustache_v${ver}/ -a sim_fraction --wp-file simScore_Minima_ElectronsOnly_updated.root -nfg 30 -q espresso --compress ${options}
 
 condor_submit condor_job.txt
 
@@ -13,15 +18,17 @@ cd ..
 mkdir ele_deepsc
 cd ele_deepsc
 mkdir error log output
-python ../condor_run_dumper.py -i /eos/cms/store/group/dpg_ecal/alca_ecalcalib/bmarzocc/Clustering/FourElectronsGunPt1-100_pythia8_StdMixing_Flat55To75_14TeV_123X_mcRun3_2021_realistic_v11_UL18_pfRechitThres_SCRegression_DeepSC_AlgoA_Dumper  -o /eos/user/r/rdfexp/ecal/cluster/output_deepcluster_dumper/reco_comparison/supercluster_regression/electrons/ele_UL18_123X_DeepSC_AlgoA_v${ver}/ -a sim_fraction --wp-file simScore_Minima_ElectronsOnly_updated.root -nfg 30 -q espresso --compress ${options}
+python ../condor_run_dumper.py -i ${basedir}/FourElectronsGunPt1-100_pythia8_StdMixing_Flat55To75_14TeV_123X_mcRun3_2021_realistic_v11_UL18_pfRechitThres_Dumper_SCRegression_DeepSC_AlgoA_125X_bugFix -o ${outdir}/electrons/ele_UL18_123X_DeepSC_AlgoA_v${ver}/ -a sim_fraction --wp-file simScore_Minima_ElectronsOnly_updated.root -nfg 30 -q espresso --compress ${options}
 
 condor_submit condor_job.txt
+
+
 
 cd ..
 mkdir gamma_must
 cd gamma_must
 mkdir error log output
-python ../condor_run_dumper.py -i /eos/cms/store/group/dpg_ecal/alca_ecalcalib/bmarzocc/Clustering/FourGammasGunPt1-100_pythia8_StdMixing_Flat55To75_14TeV_123X_mcRun3_2021_realistic_v11_UL18_pfRechitThres_SCRegression_Mustache_Dumper  -o /eos/user/r/rdfexp/ecal/cluster/output_deepcluster_dumper/reco_comparison/supercluster_regression/gammas/gamma_UL18_123X_Mustache_v${ver}/ -a sim_fraction --wp-file simScore_Minima_PhotonsOnly_updated.root -nfg 30 -q espresso --compress ${options}
+python ../condor_run_dumper.py -i ${basedir}/FourGammasGunPt1-100_pythia8_StdMixing_Flat55To75_14TeV_123X_mcRun3_2021_realistic_v11_UL18_pfRechitThres_Dumper_SCRegression_Mustache_125X_bugFix -o ${outdir}/gammas/gamma_UL18_123X_Mustache_v${ver}/ -a sim_fraction --wp-file simScore_Minima_PhotonsOnly_updated.root -nfg 30 -q espresso --compress ${options}
 
 condor_submit condor_job.txt
 
@@ -29,7 +36,7 @@ cd ..
 mkdir gamma_deepsc
 cd gamma_deepsc
 mkdir error log output
-python ../condor_run_dumper.py -i /eos/cms/store/group/dpg_ecal/alca_ecalcalib/bmarzocc/Clustering/FourGammasGunPt1-100_pythia8_StdMixing_Flat55To75_14TeV_123X_mcRun3_2021_realistic_v11_UL18_pfRechitThres_SCRegression_DeepSC_AlgoA_Dumper  -o /eos/user/r/rdfexp/ecal/cluster/output_deepcluster_dumper/reco_comparison/supercluster_regression/gammas/gamma_UL18_123X_DeepSC_AlgoA_v${ver}/ -a sim_fraction --wp-file simScore_Minima_PhotonsOnly_updated.root -nfg 30 -q espresso --compress ${options}
+python ../condor_run_dumper.py -i ${basedir}/FourGammasGunPt1-100_pythia8_StdMixing_Flat55To75_14TeV_123X_mcRun3_2021_realistic_v11_UL18_pfRechitThres_Dumper_SCRegression_DeepSC_AlgoA_125X_bugFix  -o ${outdir}/gammas/gamma_UL18_123X_DeepSC_AlgoA_v${ver}/ -a sim_fraction --wp-file simScore_Minima_PhotonsOnly_updated.root -nfg 30 -q espresso --compress ${options}
 
 condor_submit condor_job.txt
 
